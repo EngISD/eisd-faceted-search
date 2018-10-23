26171b93d8f4675904cac959a4c8e18ae76d6e1b
Igor Molnar <igor.molnar@eng.it>
2018-10-23 16:16:08 +0200

    added content-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { NgPipesModule } from 'ngx-pipes';
+// Material Modules
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatListModule} from '@angular/material/list';
 
 import { AppComponent } from './app.component';
+import { ContentListComponent } from './content-list/content-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    NgPipesModule
+    NgPipesModule,
+    // Material Modules
+    MatGridListModule,
+    MatPaginatorModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
