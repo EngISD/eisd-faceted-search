d428dffcf82288b90a99da2c9640eeaebe5a657b
Igor Molnar <igor.molnar@eng.it>
2018-10-23 17:43:05 +0200

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,30 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatListModule, MatFormFieldModule, MatIconModule, MatExpansionModule, MatSliderModule, MatInputModule } from '@angular/material';
 import { NgPipesModule } from 'ngx-pipes';
+import { FormsModule } from '@angular/forms';
 
 import { SidenavComponent } from './sidenav/sidenav.component';
 // Material Modules
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatPaginatorModule} from '@angular/material/paginator';
 
+
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
-    ContentListComponent
+    ContentListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     NgPipesModule,
+    FormsModule,
     MatButtonModule,
     MatIconModule,
     MatExpansionModule,
