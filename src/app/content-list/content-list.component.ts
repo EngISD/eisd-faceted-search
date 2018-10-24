6652113eb2b9ba9f7073ccc9817d2b2cb15d9e70
Milan Jankovic <milanjankovicsrb@gmail.com>
2018-10-24 10:39:31 +0200

    implemented frontend based search of list. (works very slow for now)

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ServiceService } from '../service.service';
 
 @Component({
@@ -8,12 +8,13 @@ import { ServiceService } from '../service.service';
 })
 export class ContentListComponent implements OnInit {
 
+   @Input() searchText: string;
   results: any;
 
   constructor(private service: ServiceService) { }
 
   ngOnInit() {
-    this.service.getResults().subscribe(res => this.results = res)
+    this.service.getResults().subscribe(res => this.results = res);
   }
 
 }
