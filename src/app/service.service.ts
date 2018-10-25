7e499176b61f094f2fad2ff50655f26611bdae08
Milan Jankovic <milanjankovicsrb@gmail.com>
2018-10-25 16:09:47 +0200

    added explanation in comment for error in service.ts code

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -23,17 +23,21 @@ export class ServiceService {
     return this.http.get(this.dataUrl + '/' + resultId);
   }
 
-  getFilterBy(data: string) {
+  /* getFilterBy(data: string) {
     return this.http.get(this.dataUrl).pipe(
       map(items => {
         return items.filter(items => items[data] > 2000);
       }, error => error)
     );
-  }
+  } */
 
   getSearchedItem(data: string) {
     return this.http.get(this.dataUrl).pipe(
       map(items => {
+        // this prints an error "Property 'filter' does not exist on type 'Object'." but works
+        // after doing some research i think it shows error because Angular 7 returns an object
+        // from http.get, on link bellow is some kind of explanation
+        // https://stackoverflow.com/questions/52989723/angular-7-http-get-returning-object-object
         return items.filter(items => items.title.toLowerCase().includes(data.toLowerCase()));
       }, error => error)
     );
