33f079ace3f844fd3bd565a5e50afd44f9ca196f
Milan Jankovic <milanjankovicsrb@gmail.com>
2018-10-29 09:35:04 +0100

    showing all options in facets

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -8,14 +8,38 @@ import { Observable } from 'rxjs';
 })
 export class ServiceService {
 
-  dataUrl: string = 'https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json';
+  dataUrl = 'https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json';
 
   // for testing of the sorting and slicing mechanism (it works)
-  //dataUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+  // dataUrl: string = 'https://jsonplaceholder.typicode.com/todos';
   constructor(private http: HttpClient) { }
-  getRawResults() {
-
-    return this.http.get(this.dataUrl);
+  getCategories() {
+    return this.http.get<Array<any>>(this.dataUrl).pipe(
+      map(items => Object.keys(items[0]) )
+    );
+  }
+  getCheckboxes(category?: string) {
+    return this.http.get<Array<any>>(this.dataUrl).pipe(
+      map(items => {
+        const temp = [];
+        for (let i = 0; i < items.length; i++) {
+          temp.push(items[i][category]);
+        }
+        const counted: Array<any> = temp.reduce(function (acc, item) {
+          if (acc[item]) {
+              acc[item]++;
+          } else {
+              acc[item] = 1;
+          }
+          return acc;
+        }, {});
+        const array = Object.keys(counted).map(function (k) {
+          return { id: k, count: counted[k]};
+        });
+        array.sort(function (a, b) { return b.count - a.count; });
+        return array.slice(0, 10);
+      })
+    );
   }
   getResults(event) {
     return this.http.get(this.dataUrl);
@@ -35,8 +59,15 @@ export class ServiceService {
   getSearchedItem(data: string) {
     return this.http.get<Array<any>>(this.dataUrl).pipe(
       map(items => {
-        return items.filter(items => items.title.toLowerCase().includes(data.toLowerCase()));
+        return items.filter(res => res.title.toLowerCase().includes(data.toLowerCase()));
       }, error => error)
     );
+  }
+
+  getParamsForFacets(category: string) {
+    return this.http.get<Array<any>>(this.dataUrl)
+            .pipe(
+              map(items => items.filter(res => res.title.toLowerCase().includes(category.toLowerCase())) )
+            );
   }
 }
