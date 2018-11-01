fe1735521386c7aebf1f4696d8e9ab7dfa8309ff
Igor Molnar <igor.molnar@eng.it>
2018-11-01 15:49:01 +0100

    small style fixes

--- src/app/demo/toolbar/toolbar.component.ts
+++ src/app/demo/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'demo-toolbar',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  // @ViewChild('searchbar') searchBar: ElementRef;
+  // @ViewChild('searchicon') searchIcon: ElementRef; 
+  // @ViewChild('input') inputEl: ElementRef;  
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
   }
+  // testF(){
+  //   this.renderer.setStyle(this.searchBar.nativeElement, 'background-color', 'rgba(16, 51, 78, 0.6)');
+  //   this.renderer.addClass(this.searchIcon.nativeElement, 'test');
+  //   this.renderer.setStyle(this.inputEl.nativeElement, 'background-color', 'rgba(16, 51, 78, 0.6)');
+  // }
+  // testF2(){
+  //   this.searchBar.nativeElement.style.backgroundColor = 'white';
+  // }
 
 }
